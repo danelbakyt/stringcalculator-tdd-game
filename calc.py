63bc5082bbfe969ce0d1003d8402b32f75d44dd2
fnm777777 <funanma@andrew.cmu.edu>
2026-06-10 22:26:34 +0800

    fixing issue for python syntax

--- calc.py
+++ calc.py
@@ -1,7 +1,7 @@
 """ Edit the function below to implement the String Calculator TDD Kata """
 def add(numbers):
-    int(numbers)
     if numbers == "":
         return 0
-    else return number
+    else:
+        return int(numbers)
     
