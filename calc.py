e00f624d8d5b0d0471676281ed1b0786d7cf6416
M-Elosta <mohamedelosta.uni@gmail.com>
2026-06-10 17:36:58 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -2,6 +2,8 @@
 def add(numbers):
     if numbers == "":
         return 0
+    elif "-" in number:
+        return -1
 
     values = numbers.split(",")
     return sum(int(value) for value in values)
